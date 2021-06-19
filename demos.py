91318ecbb46c894636ddad669b9ea740fa2ebe39
Joe Shepherd <sudo.jshepherd01@gmail.com>
2021-06-19 11:45:54 +0100

    Todo list, colours output

--- demos.py
+++ demos.py
@@ -1,3 +1,5 @@
+from utilities import *
+
 # start with the basics
 
 def selectionSort(alist):
@@ -6,6 +8,13 @@ def selectionSort(alist):
         for k in range(i, len(alist)):
             if alist[k] < alist[j]:
                 j = k
+
+        coloursList = ['c']*i + [None]*(len(alist)-i)
+        coloursList[i] = 'r'
+        coloursList[j] = 'g'
+
+        print(prettyList(alist, coloursList))
+
         temp = alist[j]
         alist[j] = alist[i]
         alist[i] = temp
---
+++ utilities.py
@@ -0,0 +1,34 @@
+# globals
+ANSIcolours = {
+    "r": "\u001b[31m",
+    "g": "\u001b[32m",
+    "b": "\u001b[34m",
+    "c": "\u001b[36m",
+    "m": "\u001b[35m",
+    "y": "\u001b[33m",
+    "reset": "\u001b[0m",
+}
+
+def colourString(astring, colour):
+    if not colour in ANSIcolours:
+        raise ValueError('Colour not supported')
+    return ANSIcolours[colour]+astring+ANSIcolours['reset']
+
+def rainbowString(astring):
+    out = ""
+    cycle = ['r','y','g','c','b','m']
+    for i in range(len(astring)):
+        out += colourString(astring[i],cycle[i % 6])
+    return out
+
+def prettyList(alist, colourslist):
+    out = "["
+    for i in range(len(alist)):
+        if len(colourslist) > i and colourslist[i] is not None:
+            out += colourString(str(alist[i]), colourslist[i])
+        else:
+            out += str(alist[i])
+        out += ', '
+    out = out[:-2]
+    out += "]"
+    return out
